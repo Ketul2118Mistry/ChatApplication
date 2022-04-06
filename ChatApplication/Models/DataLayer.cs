9ac626a6d581366ab86e01756d8c623ce9b300b5
ketul.mistry <ketul.mistry@DT094>
2022-04-06 11:55:40 +0530

    Add project files.

---
+++ ChatApplication/Models/DataLayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RabbitMQ.Client;
+using RabbitMQ.Util;
+using System.Web.Mvc;
+using ChatApplication.Models;
+using System.Web.UI.WebControls;
+
+namespace ChatApplication.Models
+{
+    public class DataLayer
+    {
+        internal Chat_tb login(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<Chat_tb> getusers(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
